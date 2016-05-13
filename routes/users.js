e9e1359d08c454e43406d0dc8d1db15c1c454379
amazingandyyy <amazingandyyy@gmail.com>
2016-05-13 01:32:01 -0700

    fix
    fix

--- routes/users.js
+++ routes/users.js
@@ -13,11 +13,11 @@ router.put('/edit', stormpath.loginRequired, function(req, res) {
     // for(var key in req.body){
     //     req.user[key] = req.body[key];
     // }
-    req.user.givenName = req.body.givenName
-    req.user.middleName = req.body.middleName
-    req.user.surname = req.body.surname
-    req.user.customData.favoriteCity = req.body.customData.favoriteCity
-    req.user.customData.favoriteFood = req.body.customData.favoriteFood
+    req.user.givenName = req.body.givenName;
+    req.user.middleName = req.body.middleName;
+    req.user.surname = req.body.surname;
+    req.user.customData.favoriteCity = req.body.customData.favoriteCity;
+    req.user.customData.favoriteFood = req.body.customData.favoriteFood;
     req.user.save((err, savedUser)=>{
         res.status(err? 400: 200).send(err || req.body);
     })
